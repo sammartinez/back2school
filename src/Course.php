92ed1db2daed901607f63c97bd27d9ff2b58465f
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-08-25 09:55:26 -0700

    Add initial files, write template for Course class

---
+++ src/Course.php
@@ -0,0 +1,90 @@
+<?php
+    class Course
+    {
+
+        private $name;
+        private $code;
+        private $id;
+
+        function __construct($name, $code, $id = null)
+        {
+
+        }
+
+
+
+
+        // Getters and Setters
+        function getName()
+        {
+
+        }
+
+        function setName($new_name)
+        {
+
+        }
+
+        function getCode()
+        {
+
+        }
+
+        function setCode($new_code)
+        {
+
+        }
+
+        function getId()
+        {
+
+        }
+
+
+
+
+
+
+        // Database storage methods
+        function save()
+        {
+
+        }
+
+        function update($new_name)
+        {
+
+        }
+
+        function delete()
+        {
+
+        }
+
+        function addStudent($student)
+        {
+
+        }
+
+        function getStudents()
+        {
+
+        }
+
+        static function getAll()
+        {
+
+        }
+
+        static function deleteAll()
+        {
+
+        }
+
+        static function find($search_id)
+        {
+            
+        }
+
+    }
+ ?>
