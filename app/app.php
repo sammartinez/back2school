a9f6736a11798bc077701c07e8b5885397388751
Mike Chastain & Sam Martinez <student@epicodus.com>
2015-08-25 15:22:33 -0700

    add twig get index and student form for post

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Stylist.php";
-    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Course.php";
+    require_once __DIR__."/../src/Student.php";
 
     $app = new Silex\Application();
     $server = 'mysql:host=localhost;dbname=back2school';
@@ -16,8 +16,24 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-    // Put your routes here dingus
+    //Get Calls
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render("index.html.twig", array('students' => Student::getAll(),'courses' => Course::getAll()));
+    });
 
+    $app->get("/students", function() use ($app) {
+        return $app['twig']->render("students.html.twig", array('students' => Student::getAll()));
+    });
+
+    //Post Calls
+    $app->post("/students", function() use ($app) {
+        $name = $_POST['name'];
+        $enroll_date = $_POST['enroll_date'];
+        $student = new Student($name, $enroll_date);
+        $student->save();
+
+        return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+    });
 
     return $app;
 ?>
